10857a6ce4f34545f9b5139d34b1f90d6ed4450d
Arie <57365299+jayzpkz@users.noreply.github.com>
2022-02-27 17:26:39 +0200

    third commit

--- smart-rental-contract/rental-contract.sol
+++ smart-rental-contract/rental-contract.sol
@@ -2,135 +2,104 @@ pragma solidity ^0.4.25;
 
 contract RentalContract {
 
+  uint public currentPeriod = 0;
+
+  enum State { Created, Started, Terminated }
+  // State public state;
+
   struct MonthlyRentalPayment {
     uint id;
     uint value; /* The amount of rent that is paid*/
   }
 
-  MonthlyRentalPayment[] public paidrents;
+  struct Apartment {
+    string apartment_address;
+    uint rent;
+    uint period;
+    address landlord;
+    address tenant;
+    State state;
+  }
 
-  uint public rentPeriod; /*period = number of months*/
-  uint public rent;
-  uint public currentPeriod;
+  Apartment public apartment = Apartment("Bazel 50/2", 10000000000000000, 12, 0x05ecf7828f8AC1f438A0c27641E696a448b86B7B, 0xea53bCA6e4e3FAd60820B427EE4426071c92a00F, State.Created);
+  mapping(uint256 => MonthlyRentalPayment) public paidrents;
 
   uint public createdTimestamp;
 
-  string public propertyAddress;
+  event contractCreated();
+  event agreementConfirmed();
+  event paidRent();
+  event contractTerminated();
 
-  address public landlord;
-  address public tenant;
-
-  enum State { Created, Started, Terminated }
-  State public state;
-
-  function RentalAgreement(uint _rentPeriod, uint _rent, string _propertyAddress) 
+  function RentalAgreement()
   public
   {
-    rentPeriod = _rentPeriod;
-    rent = _rent;
-    propertyAddress = _propertyAddress;
-    landlord = msg.sender;
-    createdTimestamp = block.timestamp;
-    currentPeriod = 1;
-
-    for(uint i = 0; i < rentPeriod; i++) {
-      paidrents.push(MonthlyRentalPayment({
-        id: i + 1,
-        value: 0
-      }));
+    for(uint i = 0; i < apartment.period; i++) {
+      paidrents[i] = (MonthlyRentalPayment({ id: i + 1, value: 0 }));
     }
-  }
-  modifier onlyLandlord() {
-    require(msg.sender == landlord);
-    _;
-  }
-  modifier onlyTenant() {
-    require(msg.sender == tenant);
-    _;
-  }
+    emit contractCreated();
+    createdTimestamp = block.timestamp;
+  } 
   modifier inState(State _state) {
-    require(state == _state);
+    require(apartment.state == _state);
     _;
   }
 
   /* We also have some getters so that we can read the values
   from the blockchain at any time */
-  function getPaidRents() internal view returns (MonthlyRentalPayment[]) {
-    return paidrents;
+  function getCurrentPeriod() public view returns (uint) {
+    if(currentPeriod == 0){
+      return 1;
+    }
+    return currentPeriod;
   }
-
-  function getpropertyAddress() public view returns (string) {
-    return propertyAddress;
-  }
-
-  function getLandlord() public view returns (address) {
-    return landlord;
-  }
-
-  function getTenant() public view returns (address) {
-    return tenant;
-  }
-
-  function getRentPeriod() public view returns (uint) {
-    return rentPeriod;
-  }
-
-  function getRent() public view returns (uint) {
-    return rent;
-  }
-
-  function getContractCreationTime() public view returns (uint) {
-    return createdTimestamp;
-  }
-
-  function getContractAddress() public view returns (address) {
-    return this;
-  }
-
-  function getState() public view returns (State) {
-    return state;
-  }
-
-  event agreementConfirmed();
-
-  event paidRent();
-
-  /*When the leas is properly terminated*/
-  event contractTerminated();
+  
 
   /* Confirm the lease agreement as tenant*/
   function confirmAgreement()
   public
+  payable
   inState(State.Created)
   {
-    require(msg.sender != landlord);
+    require(msg.sender != apartment.landlord);
+    apartment.state = State.Started;
     emit agreementConfirmed();
-    tenant = msg.sender;
-    state = State.Started;
+    apartment.tenant = msg.sender; //update the tenant to be the one who confirmed the agreement
   }
 
   function payRent()
   public
   payable
-  onlyTenant
+  // onlyTenant
   inState(State.Started)
   {
-    require(msg.value == rent);
+    require(msg.sender == apartment.tenant);
+    require(msg.value == apartment.rent);
+    require(currentPeriod < apartment.period);
     emit paidRent();
-    landlord.transfer(msg.value);
-    paidrents[currentPeriod].value = msg.value;
+    if(msg.value == apartment.rent)
+    {
+      apartment.landlord.transfer(msg.value);
+    } else {
+      apartment.landlord.transfer(1);
+    }
+    if(paidrents[currentPeriod].value == 0){
+      paidrents[currentPeriod].value = msg.value;
+    }
+    currentPeriod++;
   }
 
   /* Terminate the contract so the tenant can’t pay rent anymore,
   and the contract is terminated */
   function terminateContract()
   public
-  onlyLandlord
+  payable
   {
+    require(msg.sender == apartment.landlord);
+    apartment.state = State.Terminated;
     emit contractTerminated();
-    landlord.transfer(address(this).balance);
+    apartment.landlord.transfer(address(this).balance);
     /* If there is any value on the
     contract send it to the landlord*/
-    state = State.Terminated;
   }
 }
